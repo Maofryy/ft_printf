ae78ce065a8ab06891b8cede0845e3ce5a26bb3e
Maori Benhassine <maori.benhassine@gmail.com>
2019-02-10 22:28:26 +0100

    Updated a bit

--- ft_printf.c
+++ ft_printf.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
 
 void ft_printf(char *format, ...)
 {
     va_list ap;
-    int d;
     char c, *s;
+    int d;
 
     va_start(ap, format);
     while (*format)
     {
+      while (*format != '%') //or /0 ?
+      {
+        write(1,format,1);
+        format++;
+      }
+      format++; //Handle %% ?
       switch (*format++) {
         case 's':              /* string */
             s = va_arg(ap, char *);
@@ -22,15 +29,19 @@ void ft_printf(char *format, ...)
         case 'c':              /* char */
             /* need a cast here since va_arg only
                takes fully promoted types */
-            c = (char) va_arg(ap, int);
+            c = /*(char) */va_arg(ap, int);
             printf("char %c\n", c);
             break;
         }
+        format++;
     }
     va_end(ap);
 }
 
+//add a convert() same as atoi base
+
 int main(int ac, char **av) {
-    ft_printf("d s", 1, "Hey");
+    //ft_printf("%d %s", 1, "Hey");
+    printf("%-10s n\nHey","Hey");
     return (0);
 }
